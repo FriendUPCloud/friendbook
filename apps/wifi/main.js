757ef9406612ee60edeed6055fff1eac4488ce23
Hogne Titlestad <ht@friendup.cloud>
2023-10-30 13:54:43 +0100

    Add some app stubs for Wifi and Power apps.

---
+++ apps/wifi/main.js
@@ -0,0 +1,14 @@
+/*©mit**************************************************************************
+*                                                                              *
+* This file is part of FRIEND UNIFYING PLATFORM.                               *
+* Copyright (c) Friend Software Labs AS. All rights reserved.                  *
+*                                                                              *
+* Licensed under the Source EULA. Please refer to the copy of the MIT License, *
+* found in the file LICENSE.                                                   *
+*                                                                              *
+*****************************************************************************©*/
+
+Application.run = function( msg )
+{
+}
+
