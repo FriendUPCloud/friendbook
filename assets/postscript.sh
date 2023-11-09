2b85ea88eba50c7512126b8969c899e9dfc6cbad
Hogne Titlestad <ht@friendup.cloud>
2023-11-09 14:42:22 +0100

    Fix

---
+++ assets/postscript.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+log_file="$HOME/friendbook.log"
+
+echo "Starting friendbook session." > "$log_file"
+
+
+# Get the active window ID
+active_window_id=""
+
+# Check if there is an active window  
+while [ -z "$active_window_id" ]; do
+    active_window_id=$(xwininfo -root -tree | grep '"-root" _NET_ACTIVE_WINDOW(WINDOW)' | awk '{print $1}')
+    echo "We have a candidate: $active_window_id" >> "$log_file"
+    if [ -n "$active_window_id" ]; then
+        break  # Exit the loop when an active window ID is found
+    fi
+    sleep 1
+done
+
+echo "We have an active window." >> "$log_file"
+
+echo "Active window is $active_window_id" >> "$log_file"
+
+# Set the "CustomAction" property to the active window
+xdotool set_window --name "CustomAction" $active_window_id
+
+# Get the screen resolution
+screen_width=$(xrandr | awk '/\*/ {print $1}' | cut -d 'x' -f1)
+screen_height=$(xrandr | awk '/\*/ {print $1}' | cut -d 'x' -f2)
+
+echo "Active window dims $screen_width, $screen_height" >> "$log_file"
+
+# Move and resize the window to cover the entire screen
+xdotool windowmove $active_window_id 0 0
+xdotool windowsize $active_window_id $screen_width $screen_height
+
+# Position in the right layer
+wmctrl -i -r $active_window_id -b add,below
+wmctrl -i -a $active_window_id
+wmctrl -i -r $active_window_id -e 0,0,0,$screen_width,$screen_height
+
+echo "Done." >> "$log_file"
+
--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ cp friendos.session ~/.xsessionrc
 mkdir -p ~/.config
 mkdir -p ~/.config/openbox
 cp custom.xml ~/.config/openbox/
-
+mkdir -p ~/.config/friendbook
+cp assets/postscript.sh ~/.config/friendbook/
