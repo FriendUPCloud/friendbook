736b1424dadd41f11822f2b445a1ccd2f764ae4f
Hogne Titlestad <ht@friendup.cloud>
2023-11-09 13:25:13 +0100

    Try again

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 sudo cp friendos.desktop /usr/share/xsessions/
 cp assets/jwmrc ~/.jwmrc
 cp friendos.session ~/.xsessionrc
+mkdir -p ~/.config
+mkdir -p ~/.config/openbox
 cp custom.xml ~/.config/openbox/
 
