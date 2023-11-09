8eb1a838419b94a27ea6f4c9441d17f9ef014a7a
Hogne Titlestad <ht@friendup.cloud>
2023-11-09 15:57:57 +0100

    Rename

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ cp assets/jwmrc ~/.jwmrc
 cp friendos.session ~/.xsessionrc
 mkdir -p ~/.config
 mkdir -p ~/.config/openbox
-cp custom.xml ~/.config/openbox/
+cp custom.xml ~/.config/openbox/rc.xml
 mkdir -p ~/.config/friendbook
 cp assets/postscript.sh ~/.config/friendbook/
